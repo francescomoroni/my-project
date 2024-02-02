36a96e568a724a05fd11e385018e71fd42da51f1
marcotommoro <marcomoroni99@gmail.com>
2024-02-02 16:05:32 +0100

    feat: qualcosina

--- app/page.tsx
+++ app/page.tsx
@@ -1,83 +1,151 @@
-'use client'
-import React, { useState, useEffect } from "react";
-import Topbar from "./components/Topbar";
-import Footer from "./components/Footer";
-import { homepage } from "./data/data";
-import Image from "next/image";
-import MuxPlayer from '@mux/mux-player-react';
-
+'use client';
+import { useEffect, useState } from 'react';
+import Footer from './components/Footer';
+import Topbar from './components/Topbar';
+import { homepage } from './data/data';
 
 export default function Home() {
+	const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+	//indice del video corrente che sta playando
+	const [currentIndex, setCurrentIndex] = useState(1);
+	// quando il primo video si è caricato
+	// carica anche gli altri N video
+	const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % homepage.length);
-    }, 8000);
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoaded(true);
+		}, 1000);
+	}, []);
 
-    return () => clearInterval(interval);
-  }, []);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentImageIndex(
+				(prevIndex) => (prevIndex + 1) % homepage.length,
+			);
+		}, 8000);
 
-  const nextSlide = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % homepage.length);
-  }
+		return () => clearInterval(interval);
+	}, []);
 
-  return (
-    <>
-      {/* Header */}
-      <div className="fixed z-10 w-full">
-        <Topbar />
-      </div>
+	const nextSlide = () => {
+		setCurrentImageIndex((prevIndex) => (prevIndex + 1) % homepage.length);
+	};
 
-      {/* Video Centrale */}
-      <div className="relative h-screen w-screen bg-black overflow-hidden cursor-pointer">
-        <video autoPlay loop muted playsInline src="video.mp4" className="w-full h-full object-cover"></video>
-        
-        {/* <Image alt="" fill={true} onClick={nextSlide} className="object-cover"  src={`/${currentImageIndex}.webp`} ></Image> */}
-        
-        {/* <MuxPlayer
-          streamType="on-demand"
-          playbackId="wtcsOwZ3kuakwsJxOYl402tagjgI7ifNNLK100ZxCHTrI"
-          metadataVideoTitle="Placeholder (optional)"
-          metadataViewerUserId="Placeholder (optional)"
-          primaryColor="#FFFFFF"
-          secondaryColor="#000000"
-          metadata={{
-            video_id: "video-id-54321",
-            video_title: "Test video title",
-            viewer_user_id: "user-id-007",
-          }}
-          style={{ position: "absolute", width: "100%", height: "100%", objectFit: "cover", inset: "0px" }}
-        /> */}
-      </div>
+	return (
+		<>
+			{/* Header */}
+			<div className='fixed z-10 w-full'>
+				<Topbar />
+			</div>
 
+			{/* Video Centrale */}
+			<div
+				onClick={() => setCurrentIndex(((currentIndex + 1) % 3) + 1)}
+				className='relative w-screen h-screen overflow-hidden bg-black cursor-pointer'
+			>
+				<div className='absolute inset-0 bg-black/40'></div>
 
-      {/* Titolo video */}
-      <h1 onClick={nextSlide} className="fixed right-6 bottom-1/3 text-right sm:right-10 sm:top-1/2 text-white hover:text-white/80  uppercase sm:text-2xl ">{homepage[currentImageIndex].title}</h1>
+				<video
+					src='video.mp4'
+					autoPlay
+					loop
+					muted
+					playsInline
+					className={`object-cover w-full h-full ${
+						currentIndex !== 1 ? 'hidden' : ''
+					}`}
+				></video>
 
-      {/* Footer */}
-      <div className="w-full fixed bottom-0">
-        <Footer />
-      </div>
+				{isLoaded ? (
+					<>
+						<video
+							src='video2.mp4'
+							autoPlay
+							loop
+							muted
+							playsInline
+							className={`object-cover w-full h-full ${
+								currentIndex !== 2 ? 'hidden' : ''
+							}`}
+						></video>
 
-    </>
-  );
+						<video
+							src='video3.mp4'
+							autoPlay
+							loop
+							muted
+							playsInline
+							className={`object-cover w-full h-full ${
+								currentIndex !== 3 ? 'hidden' : ''
+							}`}
+						></video>
+					</>
+				) : null}
+
+				{/* <video
+					autoPlay
+					loop
+					preload='none'
+					muted
+					playsInline
+					src='video2.mp4'
+					className='object-cover w-full h-full'
+				></video> */}
+
+				{/* <Image alt="" fill={true} onClick={nextSlide} className="object-cover"  src={`/${currentImageIndex}.webp`} ></Image> */}
+
+				{/* <MuxPlayer
+					streamType='on-demand'
+					playbackId='wtcsOwZ3kuakwsJxOYl402tagjgI7ifNNLK100ZxCHTrI'
+					metadataVideoTitle='Placeholder (optional)'
+					metadataViewerUserId='Placeholder (optional)'
+					primaryColor='#FFFFFF'
+					secondaryColor='#000000'
+					className='object-cover w-full h-full'
+					metadata={{
+						video_id: 'video-id-54321',
+						video_title: 'Test video title',
+						viewer_user_id: 'user-id-007',
+					}}
+					style={{}}
+				/> */}
+			</div>
+
+			{/* Titolo video */}
+			<h1
+				onClick={nextSlide}
+				className='fixed text-right text-white uppercase right-6 bottom-1/3 sm:right-10 sm:top-1/2 hover:text-white/80 sm:text-2xl '
+			>
+				{homepage[currentImageIndex].title}
+			</h1>
+
+			{/* Footer */}
+			<div className='fixed bottom-0 w-full'>
+				<Footer />
+			</div>
+		</>
+	);
 }
 
 //"bg-[url('/img/hero-pattern.svg')]"
 
-// <video poster="/logo.png" onClick={nextSlide} autoPlay loop muted playsInline src={`${currentImageIndex}.mp4`} className="w-full h-full object-cover"></video>
+// <video poster="/logo.png" onClick={nextSlide} autoPlay loop muted playsInline src={`${currentImageIndex}.mp4`} className="object-cover w-full h-full"></video>
 // src="https://player.vimeo.com/video/694426484?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
 
-{/*<iframe
+{
+	/*<iframe
           onClick={nextSlide}
           title={homepage[currentImageIndex].title}
           src={`https://player.vimeo.com/video/${homepage[currentImageIndex].srcVideo}?autoplay=1&loop=1&muted=1&controls=0&#t=0m0s&app_id=58479&quality=240p`}
           style={{ position:"absolute", width: "100%", height: "100%", objectFit: "cover", inset: "0px", border: "none"}}
           width={"100%"} height={"100%"} frameBorder="0"
-        ></iframe>*/}
+        ></iframe>*/
+}
 
-{/* <div className="relative h-screen w-screen bg-gray-300 overflow-hidden bg-[url('https://player.vimeo.com/video/694426484?background=1&app_id=58479')]">
-       
-      </div>  */}
+{
+	/* <div className="relative h-screen w-screen bg-gray-300 overflow-hidden bg-[url('https://player.vimeo.com/video/694426484?background=1&app_id=58479')]">
+
+      </div>  */
+}
--- app/pages/works/page.tsx
+++ app/pages/works/page.tsx
@@ -1,40 +1,51 @@
-import Link from "next/link";
-import { works } from "../../data/data";
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
+import { works } from '../../data/data';
 
 export default function Home() {
+	return (
+		<>
+			<div className='grid grid-cols-1 pb-40 mx-4 my-8 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-24 xl:gap-x-20 xl:gap-y-32 sm:mx-8'>
+				{works.map((work, key) => {
+					return (
+						<div className='flex flex-col' key={key}>
+							<Link
+								href={`/pages/works/work/${work.linkPage}`}
+								className='relative mx-auto h-[18rem] xl:h-[24rem] w-full group'
+							>
+								<Image
+									className='object-cover w-full h-full transition duration-300 ease-in-out transform shadow-2xl group-hover:shadow-3xl '
+									fill
+									sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+									blurDataURL={work.srcWorkPageImg}
+									src={work.srcWorkPageImg}
+									alt={work.titleWorkPage}
+								/>
+							</Link>
 
-
-  return (
-    <>
-      <div className="pb-40 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-24 xl:gap-x-20 xl:gap-y-32 my-8 mx-4 sm:mx-8">
-        {works.map((work, key) => {
-          return (
-              <Link href={`/pages/works/work/${work.linkPage}`} key={key} className="relative mx-auto h-[18rem] xl:h-[24rem] w-full group ">
-                <Image
-                  className="w-full h-full object-cover shadow-2xl group-hover:shadow-3xl transition duration-300 ease-in-out transform "
-                  fill
-                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  blurDataURL={work.srcWorkPageImg}
-                  src={work.srcWorkPageImg}
-                  alt={work.titleWorkPage} />
-                <h1 className="group-hover:line-through transition-all duration-300 ease-in-out transform pt-8 pl-2  uppercase cursor-pointer sm:text-lg md:text-xl ">{work.titleWorkPage}</h1>
-              </Link>
-          )
-        }
-        )}
-      </div>
-
-    </>
-
-  )
+							<Link
+								href={`/pages/works/work/${work.linkPage}`}
+								key={key}
+							>
+								<h1 className='pt-8 pl-2 uppercase transition-all duration-300 ease-in-out transform cursor-pointer group-hover:line-through sm:text-lg md:text-xl'>
+									{work.titleWorkPage}
+								</h1>
+							</Link>
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
 }
 
-
-
-{/* <img className="transition cursor-pointer object-cover w-full max-w-2xl aspect-video sm:aspect-square shadow-lg hover:shadow-xl" src={work.srcWorkPageImg} alt={work.titleWorkPage} /> */ }
-{/* <iframe
+{
+	/* <img className="object-cover w-full max-w-2xl transition shadow-lg cursor-pointer aspect-video sm:aspect-square hover:shadow-xl" src={work.srcWorkPageImg} alt={work.titleWorkPage} /> */
+}
+{
+	/* <iframe
                 title={work.title}
                 src={`https://player.vimeo.com/video/${work.src}?autoplay=0&loop=1&muted=1&controls=0&#t=0m0s&app_id=58479&quality=240p`}
                 style={{ width: "100%", height: "100%", objectFit: "cover" }}
-              ></iframe> */}
+              ></iframe> */
+}
